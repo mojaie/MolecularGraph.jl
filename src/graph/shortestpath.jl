8edf35573ed10443507acad472c598a0c6c75373
Seiji Matsuoka <mojaie@aol.com>
2019-03-14 16:13:44 +0900

    eccentricity and diameter

--- src/graph/shortestpath.jl
+++ src/graph/shortestpath.jl
@@ -5,15 +5,20 @@
 
 export
     shortestpath,
-    reachablenodes,
-    pathlength,
-    backtrack
+    distance, distancematrix,
+    eccentricity, diameter,
+    longestshortestpath,
+    reachablenodes, pathlength, backtrack
 
 
+"""
+    shortestpath(graph::UDGraph, u, v) -> Vector{Int}
+
+Compute the shortest path between `u` and `v` as a vector of the nodes that
+starts with `u` and ends with`v`. Return nothing if u == v or not reachable.
+"""
 function shortestpath(graph::UDGraph, u, v)
-    if u == v
-        return
-    end
+    u == v && return
     queue = [u]
     pred = Dict{Int,Union{Int,Nothing}}(u => nothing)
     while !isempty(queue)
@@ -37,6 +42,91 @@ function shortestpath(graph::UDGraph, u, v)
     end
     # return nothing if not reachable
 end
+
+
+"""
+    distance(graph::UDGraph, root) -> Dict{Int,Union{Int,Nothing}}
+
+Compute the distance from `root` to any other nodes. If the nodes are not
+reachable each other, the value will be `nothing`.
+"""
+function distance(graph::UDGraph, root)
+    queue = [root]
+    dist = Dict{Int,Union{Int,Nothing}}(root => 0)
+    while !isempty(queue)
+        q = popfirst!(queue)
+        for nbr in neighborkeys(graph, q)
+            if !(nbr in keys(dist))
+                dist[nbr] = dist[q] + 1
+                push!(queue, nbr)
+            end
+        end
+    end
+    return dist
+end
+
+
+"""
+    distancematrix(graph::UDGraph) -> Matrix{Float64}
+
+Compute the distance among each other nodes.
+
+Note that the type of the generated matrix will be `Float64`. If the nodes are
+not reachable each other, the distance value will be `Inf`.
+"""
+function distancematrix(graph::UDGraph)
+    ncnt = nodecount(graph)
+    distmat = zeros((ncnt, ncnt))
+    for s in nodekeys(graph) # TODO: should be ordered
+        for (t, d) in distance(graph, s)
+            distmat[s, t] = d === nothing ? Inf : d
+        end
+    end
+    return distmat
+end
+
+
+"""
+    eccentricity(graph::UDGraph, v) => Int
+
+Compute the eccentricity of the graph (the largest distance between `v` and
+any other nodes).
+"""
+eccentricity(graph::UDGraph, v) = maximum(values(distance(graph, v)))
+
+
+"""
+    diameter(graph::UDGraph) => Int
+
+Compute the diameter of the graph (the largest eccentricity of any nodes).
+"""
+diameter(graph::UDGraph) = maximum(
+    eccentricity(graph, n) for n in nodekeys(graph))
+
+
+"""
+    longestshortestpath(graph::UDGraph) -> Vector{Int}
+
+Compute the longest shortest path in the graph (a path between two arbitrary
+peripheral nodes) as a vector of nodes that starts with one of the
+peripheral node and ends with the other side.
+"""
+function longestshortestpath(graph::UDGraph)
+    otherside = Dict{Int,Int}()
+    eccent = Dict{Int,Int}()
+    for v in nodekeys(graph)
+        d = distance(graph, v)
+        farthest = sort(collect(keys(d)), by=k->d[k])[end]
+        otherside[v] = farthest
+        eccent[v] = d[farthest]
+    end
+    peripheral = sort(collect(keys(eccent)), by=k->d[k])[end]
+    return shortestpath(graph, peripheral, otherside[peripheral])
+end
+
+
+
+# TODO: refactor DGraph methods
 
 
 function reachablenodes(graph::DGraph, source)
