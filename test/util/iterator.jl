fde27a54aa7b9d22562e54fbbb48b42de69d48d2
Tim Holy <tim.holy@gmail.com>
2021-08-19 17:46:50 -0500

    Generalize exception type
    In preparation for https://github.com/JuliaLang/julia/pull/41885

--- test/util/iterator.jl
+++ test/util/iterator.jl
@@ -19,7 +19,7 @@ end
     @test sortstablemax([1, 4, 2, 7, 5]) == 7
     @test sortstablemax([8:10, 1:3, [], 4:23, 1:9], by=length) == 4:23
     @test sortstablemax([8:10, 1:3, 3:22, 4:23, 1:9], by=length) == 3:22
-    @test_throws ArgumentError sortstablemax([])
+    @test_throws Union{ArgumentError,MethodError} sortstablemax([])
     @test sortstablemax([], init=0) == 0
 end
 
@@ -27,7 +27,7 @@ end
     @test sortstablemin([1, 4, 2, 7, 5]) == 1
     @test sortstablemin([8:10, 1:3, [], 4:23, 1:9], by=length) == []
     @test sortstablemin([8:10, 1:3, 3:22, 4:23, 1:9], by=length) == 8:10
-    @test_throws ArgumentError sortstablemin([])
+    @test_throws Union{ArgumentError,MethodError} sortstablemin([])
     @test sortstablemin([], init=0) == 0
 end
 end # util.iterator
