08661471e0e8dc1ace8663ac2ff02c1f60d84dce
Helmut Hänsel <helmut.haensel@gmx.com>
2023-06-14 17:25:58 +0200

    add sdf and mol generation from inchi string

--- test/inchi.jl
+++ test/inchi.jl
@@ -10,4 +10,11 @@
     ikey2 = inchikey(inchi2)
     @test inchi1 == inchi2
     @test ikey1 == ikey2
+    mol_from_inchi = inchitomol(inchi1)
+    inchi1_no_stereo = inchi(txt, options = "SNon")
+    inchi3 = inchi(mol_from_inchi)
+    # lbinchi version 1.05 doesn't support chiral sdf output, therefore test without stereo information
+    # as soon as version 1.06 is in place, the lines below need to be adapted
+    @test_broken inchi3 == inchi1
+    @test inchi3 == inchi1_no_stereo
 end
