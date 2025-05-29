b8ea59f71c485594afe6280b2b3c90204ef7c8e4
Seiji Matsuoka <ms0ae8ti1sj2ui@gmail.com>
2025-05-29 12:43:45 +0900

    RDKit fingerprints

--- src/rdkit.jl
+++ src/rdkit.jl
@@ -4,18 +4,19 @@
 #
 
 export
-    moltosmiles, moltosmarts, to_rdkmol, to_rdkqmol
+    to_rdkmol, moltosmiles,
+    morgan_fp_vector, rdkit_fp_vector,
+    pattern_fp_vector, atom_pair_fp_vector,
+    topological_torsion_fp_vector
 
-using RDKitMinimalLib: get_mol, get_qmol, get_smiles
+using RDKitMinimalLib: Mol, get_mol, get_smiles,
+    get_morgan_fp_as_bytes, get_rdkit_fp_as_bytes,
+    get_pattern_fp_as_bytes, get_atom_pair_fp_as_bytes,
+    get_topological_torsion_fp_as_bytes
 
 
 function to_rdkmol(mol::MolGraph)
     return get_mol(to_json(Val{:rdkit}(), mol))
-end
-
-
-function to_rdkqmol(mol::MolGraph{Int,QueryTree,QueryTree})
-    return get_qmol(to_json(Val{:rdkit}(), mol))
 end
 
 
@@ -24,6 +25,42 @@ function moltosmiles(mol::MolGraph, details=nothing)
 end
 
 
-function moltosmarts(mol::MolGraph{Int,QueryTree,QueryTree}, details=nothing)
-    return get_smiles(to_rdkmol(mol), details)
+function uint8vec_to_bitarray(uvec::Vector{UInt8})
+    bits = BitVector(undef, 8 * length(uvec))
+    for (i, byte) in enumerate(uvec)
+        for j in 0:7
+            bits[8*(i-1) + j + 1] = (byte >> (7 - j)) & 0x01 == 1
+        end
+    end
+    return bits
 end
+
+
+morgan_fp_vector(mol::Mol; kwargs...
+    ) = uint8vec_to_bitarray(get_morgan_fp_as_bytes(mol::Mol, kwargs...))
+morgan_fp_vector(mol::MolGraph; kwargs...
+    ) = morgan_fp_vector(to_rdkmol(mol); kwargs...)
+
+rdkit_fp_vector(mol::Mol; kwargs...
+    ) = uint8vec_to_bitarray(get_rdkit_fp_as_bytes(mol::Mol, kwargs...))
+rdkit_fp_vector(mol::MolGraph; kwargs...
+    ) = rdkit_fp_vector(to_rdkmol(mol); kwargs...)
+
+pattern_fp_vector(mol::Mol; kwargs...
+    ) = uint8vec_to_bitarray(get_pattern_fp_as_bytes(mol::Mol, kwargs...))
+pattern_fp_vector(mol::MolGraph; kwargs...
+    ) = pattern_fp_vector(to_rdkmol(mol); kwargs...)
+
+atom_pair_fp_vector(mol::Mol; kwargs...
+    ) = uint8vec_to_bitarray(get_atom_pair_fp_as_bytes(mol::Mol, kwargs...))
+atom_pair_fp_vector(mol::MolGraph; kwargs...
+    ) = atom_pair_fp_vector(to_rdkmol(mol); kwargs...)
+
+topological_torsion_fp_vector(mol::Mol; kwargs...
+    ) = uint8vec_to_bitarray(get_topological_torsion_fp_as_bytes(mol::Mol, kwargs...))
+topological_torsion_fp_vector(mol::MolGraph; kwargs...
+    ) = topological_torsion_fp_vector(to_rdkmol(mol); kwargs...)
+
+
+jaccard_index(a::BitVector, b::BitVector
+    ) = sum(a .& b) / sum(a.| b)  # Not exported
