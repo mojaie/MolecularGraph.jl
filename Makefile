f486f2ad08959213ac3747f4a5195fa8a11166c9
Seiji Matsuoka <ms0ae8ti1sj2ui@gmail.com>
2025-06-05 13:02:48 +0900

    commandline package build with juliac.jl

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
-.PHONY: docs
+JULIA ?= $(shell which julia)
+JULIAC ?= $(shell $(JULIA) -e 'print(normpath(joinpath(Sys.BINDIR, Base.DATAROOTDIR, "julia", "juliac.jl")))')
+OUTPUT := /opt/moleculargraphjl/libmoleculargraph.dylib
+.PHONY: docs, clean, build
+
 docs:
 	@julia --project=docs/ --color=yes docs/make.jl
+
+clean:
+	rm -rf $(OUTPUT)
+
+build: clean
+	$(JULIA) --project=. $(JULIAC) --experimental --output-lib $(OUTPUT) --trim=safe --compile-ccallable src/MolecularGraph.jl
--- scripts/python_test.py
+++ scripts/python_test.py
@@ -9,6 +9,14 @@ import platform
 
 def jl_init():
     dlext = "dylib" if platform.system() == "Darwin" else "so"
+    libdir = Path("/opt/moleculargraphjl")
+    jl = CDLL(libdir / f"libmoleculargraph.{dlext}", RTLD_GLOBAL)
+    jl.jl_init_with_image_file(bytes(libdir), f"libmoleculargraph.{dlext}".encode())
+    return jl
+
+
+def jl_init_pc():  # For PackageCompiler. TODO: To be removed.
+    dlext = "dylib" if platform.system() == "Darwin" else "so"
     libdir = Path("/opt/moleculargraphjl/lib")
     jl = CDLL(libdir / f"libmoleculargraph.{dlext}", RTLD_GLOBAL)
     jl.jl_init_with_image(bytes(libdir), f"libmoleculargraph.{dlext}".encode())
