1771ad738a9477723a014dd8b087787dfbd9125b
Seiji Matsuoka <ms0ae8ti1sj2ui@gmail.com>
2025-06-30 06:27:47 +0900

    wip

--- build/generate_precompile.jl
+++ build/generate_precompile.jl
@@ -12,6 +12,8 @@ using Cairo
 using JSON
 using MolecularGraph
 
+include("libmoleculargraph.jl")
+
 function run()
     op = JSON.json(Dict())
     paclitaxel_str = "CC1=C2[C@@]([C@]([C@H]([C@@H]3[C@]4([C@H](OC4)C[C@@H]([C@]3(C(=O)[C@@H]2OC(=O)C)C)O)OC(=O)C)OC(=O)c5ccccc5)(C[C@@H]1OC(=O)[C@H](O)[C@@H](NC(=O)c6ccccc6)c7ccccc7)O)(C)C"
@@ -77,7 +79,6 @@ function run()
 
     notamide = unsafe_string(smartstomol(
         unsafe_convert(Cstring, raw"[NX3;H2,H1;!$(NC=O)]")))
-    @info notamide
     notamide2 = unsafe_string(smartstomol(
         unsafe_convert(Cstring, raw"[NX3;H2,H1]")))
     rotatable = unsafe_string(smartstomol(
