39b0ddaa4c528e67d574ec59283d0ad989f285be
Seiji Matsuoka <ms0ae8ti1sj2ui@gmail.com>
2025-12-25 14:19:20 +0900

    improve stability of C string

--- build/src/LibMolGraphJL.jl
+++ build/src/LibMolGraphJL.jl
@@ -13,33 +13,46 @@ using JSON
 using MolecularGraph
 
 
+function julia_strdup(s::String)::Cstring
+    # Allocate String on the C side
+    n = sizeof(s) + 1
+    p = Base.Libc.malloc(n)
+    p == C_NULL && error("malloc failed")
+    unsafe_copyto!(Ptr{UInt8}(p), pointer(s), n)
+    return Cstring(p)
+end
+
+
 Base.@ccallable function smilestomol(smiles::Cstring, options::Cstring)::Cstring
     try
-        mol = MolecularGraph.smilestomol(unsafe_string(smiles))
-        op = JSON.parse(unsafe_string(options))
+        smstr = unsafe_string(smiles)  # explicit copy
+        opstr = unsafe_string(options)  # explicit copy
+        mol = MolecularGraph.smilestomol(smstr)
+        op = JSON.parse(opstr)
         if haskey(op, "extract_largest_component") && op["extract_largest_component"]
             extract_largest_component!(mol)  # default extract_largest_component=false
         end
         if haskey(op, "remove_all_hydrogens") && op["remove_all_hydrogens"]
             remove_all_hydrogens!(mol)  # default remove_all_hydrogens=false
         end
-        unsafe_convert(Cstring, JSON.json(mol))
+        julia_strdup(JSON.json(mol))
     catch e
         mol = SMILESMolGraph()
         mol[:logs]["error_smiles"] = e.msg
-        unsafe_convert(Cstring, JSON.json(mol))
+        julia_strdup(JSON.json(mol))
     end
 end
 
 
 Base.@ccallable function smartstomol(smarts::Cstring)::Cstring
     try
-        mol = MolecularGraph.smartstomol(unsafe_string(smarts))
-        unsafe_convert(Cstring, JSON.json(mol))
+        smstr = unsafe_string(smarts)  # explicit copy
+        mol = MolecularGraph.smartstomol(smstr)
+        julia_strdup(JSON.json(mol))
     catch e
         mol = SMARTSMolGraph()
         mol[:logs]["error_smarts"] = e.msg
-        unsafe_convert(Cstring, JSON.json(mol))
+        julia_strdup(JSON.json(mol))
     end
 end
 
@@ -47,26 +60,29 @@ end
 Base.@ccallable function sdftomol(sdf::Cstring, options::Cstring)::Cstring
     try
         # return empty mol on error
-        mol = MolecularGraph.sdftomol(IOBuffer(unsafe_string(sdf)))
-        op = JSON.parse(unsafe_string(options))
+        sdfstr = unsafe_string(sdf)  # explicit copy
+        opstr = unsafe_string(options)  # explicit copy
+        mol = MolecularGraph.sdftomol(IOBuffer(sdfstr))
+        op = JSON.parse(opstr)
         if haskey(op, "extract_largest_component") && op["extract_largest_component"]
             extract_largest_component!(mol)  # default extract_largest_component=false
         end
         if haskey(op, "remove_all_hydrogens") && op["remove_all_hydrogens"]
             remove_all_hydrogens!(mol)  # default remove_all_hydrogens=false
         end
-        unsafe_convert(Cstring, JSON.json(mol))
+        julia_strdup(JSON.json(mol))
     catch e
         mol = SDFMolGraph()
         mol[:logs]["error_sdfile"] = e.msg
-        unsafe_convert(Cstring, JSON.json(mol))
+        julia_strdup(JSON.json(mol))
     end
 end
 
 
 Base.@ccallable function vertex_count(mol::Cstring)::Cint
     try
-        molobj = mol_from_json(unsafe_string(mol))
+        mstr = unsafe_string(mol)  # explicit copy
+        molobj = mol_from_json(mstr)
         nv(molobj)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
@@ -76,7 +92,8 @@ end
 
 Base.@ccallable function edge_count(mol::Cstring)::Cint
     try
-        molobj = mol_from_json(unsafe_string(mol))
+        mstr = unsafe_string(mol)  # explicit copy
+        molobj = mol_from_json(mstr)
         ne(molobj)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
@@ -86,9 +103,10 @@ end
 
 Base.@ccallable function inchikey(mol::Cstring)::Cstring
     try
-        molobj = mol_from_json(unsafe_string(mol))
+        mstr = unsafe_string(mol)  # explicit copy
+        molobj = mol_from_json(mstr)
         ikey = MolecularGraph.inchikey(molobj)
-        unsafe_convert(Cstring, something(ikey, ""))
+        julia_strdup(something(ikey, ""))
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -97,7 +115,8 @@ end
 
 Base.@ccallable function standard_weight(mol::Cstring)::Cdouble
     try
-        molobj = mol_from_json(unsafe_string(mol))
+        mstr = unsafe_string(mol)  # explicit copy
+        molobj = mol_from_json(mstr)
         MolecularGraph.standard_weight(molobj, 2)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
@@ -107,8 +126,10 @@ end
 
 Base.@ccallable function molblock(mol::Cstring)::Cstring
     try
-        molobj = mol_from_json(unsafe_string(mol))
-        unsafe_convert(Cstring, printv2mol(molobj; givebackhydrogen=false))
+        mstr = unsafe_string(mol)  # explicit copy
+        molobj = mol_from_json(mstr)
+        molb = printv2mol(molobj; givebackhydrogen=false)
+        julia_strdup(molb)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -117,12 +138,13 @@ end
 
 Base.@ccallable function sdfmolblock(mol::Cstring)::Cstring
     try
-        molobj = mol_from_json(unsafe_string(mol))
+        mstr = unsafe_string(mol)  # explicit copy
+        molobj = mol_from_json(mstr)
         buf = IOBuffer(write=true)
         printv2sdf(buf, molobj; givebackhydrogen=false)
         res = String(take!(buf))
         close(buf)
-        unsafe_convert(Cstring, res)
+        julia_strdup(res)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -131,15 +153,17 @@ end
 
 Base.@ccallable function drawsvg(mol::Cstring, options::Cstring)::Cstring
     try
-        molobj = mol_from_json(unsafe_string(mol))
-        op = JSON.parse(unsafe_string(options))
+        mstr = unsafe_string(mol)  # explicit copy
+        opstr = unsafe_string(options)  # explicit copy
+        molobj = mol_from_json(mstr)
+        op = JSON.parse(opstr)
         kwgs = Pair{Symbol,Any}[]
         haskey(op, "viewbox") && push!(kwgs, :viewbox => op["viewbox"])
         haskey(op, "show_carbon") && push!(kwgs, :show_carbon => Symbol(op["show_carbon"]))
         haskey(op, "bgcolor") && push!(kwgs, :bgcolor => op["bgcolor"])
         haskey(op, "bgopacity") && push!(kwgs, :bgopacity => op["bgopacity"])
         svg = MolecularGraph.drawsvg(molobj; kwgs...)
-        unsafe_convert(Cstring, svg)
+        julia_strdup(svg)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -149,8 +173,10 @@ end
 Base.@ccallable function drawpng(
         mol::Cstring, width::UInt32, height::UInt32, options::Cstring)::Cstring
     try
-        molobj = mol_from_json(unsafe_string(mol))
-        op = JSON.parse(unsafe_string(options))
+        mstr = unsafe_string(mol)  # explicit copy
+        opstr = unsafe_string(options)  # explicit copy
+        molobj = mol_from_json(mstr)
+        op = JSON.parse(opstr)
         kwgs = Pair{Symbol,Any}[]
         haskey(op, "show_carbon") && push!(kwgs, :show_carbon => Symbol(op["show_carbon"]))
         haskey(op, "bgcolor") && push!(kwgs, :bgcolor => op["bgcolor"])
@@ -161,7 +187,7 @@ Base.@ccallable function drawpng(
         close(iob64_encode)
         str = String(take!(buf))
         close(buf)
-        unsafe_convert(Cstring, str)
+        julia_strdup(str)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -171,10 +197,13 @@ end
 Base.@ccallable function has_exact_match(
         mol1::Cstring, mol2::Cstring, kwargs::Cstring)::Cint
     try
-        mol1 = mol_from_json(unsafe_string(mol1))
-        mol2 = mol_from_json(unsafe_string(mol2))
-        kwargs = JSON.parse(unsafe_string(kwargs))
-        MolecularGraph.has_exact_match(mol1, mol2; kwargs...)
+        mstr1 = unsafe_string(mol1)  # explicit copy
+        mstr2 = unsafe_string(mol2)  # explicit copy
+        kwstr = unsafe_string(kwargs)  # explicit copy
+        molobj1 = mol_from_json(mstr1)
+        molobj2 = mol_from_json(mstr2)
+        kw = JSON.parse(kwstr)
+        MolecularGraph.has_exact_match(molobj1, molobj2; kw...)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -184,10 +213,13 @@ end
 Base.@ccallable function has_substruct_match(
         mol1::Cstring, mol2::Cstring, kwargs::Cstring)::Cint
     try
-        mol1 = mol_from_json(unsafe_string(mol1))
-        mol2 = mol_from_json(unsafe_string(mol2))
-        kwargs = JSON.parse(unsafe_string(kwargs))
-        MolecularGraph.has_substruct_match(mol1, mol2; kwargs...)
+        mstr1 = unsafe_string(mol1)  # explicit copy
+        mstr2 = unsafe_string(mol2)  # explicit copy
+        kwstr = unsafe_string(kwargs)  # explicit copy
+        molobj1 = mol_from_json(mstr1)
+        molobj2 = mol_from_json(mstr2)
+        kw = JSON.parse(kwstr)
+        MolecularGraph.has_substruct_match(molobj1, molobj2; kw...)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -197,10 +229,13 @@ end
 Base.@ccallable function tdmcis_size(
         mol1::Cstring, mol2::Cstring, kwargs::Cstring)::Cint
     try
-        mol1 = mol_from_json(unsafe_string(mol1))
-        mol2 = mol_from_json(unsafe_string(mol2))
-        kwargs = Dict(Symbol(k) => v for (k, v) in JSON.parse(unsafe_string(kwargs)))
-        length(tdmcis(mol1, mol2; kwargs...)[1])
+        mstr1 = unsafe_string(mol1)  # explicit copy
+        mstr2 = unsafe_string(mol2)  # explicit copy
+        kwstr = unsafe_string(kwargs)  # explicit copy
+        molobj1 = mol_from_json(mstr1)
+        molobj2 = mol_from_json(mstr2)
+        kw = Dict(Symbol(k) => v for (k, v) in JSON.parse(kwstr))
+        length(tdmcis(molobj1, molobj2; kw...)[1])
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -210,10 +245,13 @@ end
 Base.@ccallable function tdmces_size(
         mol1::Cstring, mol2::Cstring, kwargs::Cstring)::Cint
     try
-        mol1 = mol_from_json(unsafe_string(mol1))
-        mol2 = mol_from_json(unsafe_string(mol2))
-        kwargs = Dict(Symbol(k) => v for (k, v) in JSON.parse(unsafe_string(kwargs)))
-        length(tdmces(mol1, mol2; kwargs...)[1])
+        mstr1 = unsafe_string(mol1)  # explicit copy
+        mstr2 = unsafe_string(mol2)  # explicit copy
+        kwstr = unsafe_string(kwargs)  # explicit copy
+        molobj1 = mol_from_json(mstr1)
+        molobj2 = mol_from_json(mstr2)
+        kw = Dict(Symbol(k) => v for (k, v) in JSON.parse(kwstr))
+        length(tdmces(molobj1, molobj2; kw...)[1])
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
     end
@@ -223,17 +261,20 @@ end
 Base.@ccallable function tdmcis_gls(
         mol1::Cstring, mol2::Cstring, kwargs::Cstring)::Cdouble
     try
-        mol1 = mol_from_json(unsafe_string(mol1))
-        mol2 = mol_from_json(unsafe_string(mol2))
-        if nv(mol1) == 0 || nv(mol2) == 0
+        mstr1 = unsafe_string(mol1)  # explicit copy
+        mstr2 = unsafe_string(mol2)  # explicit copy
+        kwstr = unsafe_string(kwargs)  # explicit copy
+        molobj1 = mol_from_json(mstr1)
+        molobj2 = mol_from_json(mstr2)
+        if nv(molobj1) == 0 || nv(molobj2) == 0
             return 0.0
         end
-        kwargs = Dict(Symbol(k) => v for (k, v) in JSON.parse(unsafe_string(kwargs)))
-        mol1_ = tdmcis_constraints(mol1; kwargs...)
-        mol2_ = tdmcis_constraints(mol2; kwargs...)
+        kw = Dict(Symbol(k) => v for (k, v) in JSON.parse(kwstr))
+        mol1_ = tdmcis_constraints(molobj1; kw...)
+        mol2_ = tdmcis_constraints(molobj2; kw...)
         m1max = length(maximum_clique(SimpleGraph(Edge.(mol1_.pairs)))[1])
         m2max = length(maximum_clique(SimpleGraph(Edge.(mol2_.pairs)))[1])
-        comm = length(maximum_common_subgraph(mol1_, mol2_; kwargs...)[1])
+        comm = length(maximum_common_subgraph(mol1_, mol2_; kw...)[1])
         comm / (m1max + m2max - comm)
     catch
         Base.invokelatest(Base.display_error, Base.catch_stack())
@@ -244,14 +285,17 @@ end
 Base.@ccallable function tdmces_gls(
         mol1::Cstring, mol2::Cstring, kwargs::Cstring)::Cdouble
     try
-        mol1 = mol_from_json(unsafe_string(mol1))
-        mol2 = mol_from_json(unsafe_string(mol2))
-        if ne(mol1) == 0 || ne(mol2) == 0
+        mstr1 = unsafe_string(mol1)  # explicit copy
+        mstr2 = unsafe_string(mol2)  # explicit copy
+        kwstr = unsafe_string(kwargs)  # explicit copy
+        molobj1 = mol_from_json(mstr1)
+        molobj2 = mol_from_json(mstr2)
+        if nv(molobj1) == 0 || nv(molobj2) == 0
             return 0.0
         end
-        kwargs = Dict(Symbol(k) => v for (k, v) in JSON.parse(unsafe_string(kwargs)))
-        mol1_ = tdmces_constraints(mol1; kwargs...)
-        mol2_ = tdmces_constraints(mol2; kwargs...)
+        kwargs = Dict(Symbol(k) => v for (k, v) in JSON.parse(kwstr))
+        mol1_ = tdmces_constraints(molobj1; kwargs...)
+        mol2_ = tdmces_constraints(molobj2; kwargs...)
         m1max = length(maximum_clique(SimpleGraph(Edge.(mol1_.pairs)))[1])
         m2max = length(maximum_clique(SimpleGraph(Edge.(mol2_.pairs)))[1])
         comm = length(maximum_common_subgraph(mol1_, mol2_; kwargs...)[1])
