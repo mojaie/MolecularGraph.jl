10faf7eabbb62760785f5aa14b13b62a5ffe9d72
Seiji Matsuoka <ms0ae8ti1sj2ui@gmail.com>
2026-05-11 17:18:26 +0900

    PackagCompiler workaround for link error

--- build/build.jl
+++ build/build.jl
@@ -1,10 +1,20 @@
 
 using PackageCompiler
+import PackageCompiler
 
 target_dir = "/opt/julia/libmolgraphjl"
 # target_dir = replace(target_dir, "\\"=>"/")  # Change Windows paths to use "/"
 
 println("Creating library in $target_dir")
+
+# TODO: PackageCompiler workaround for link error
+
+function PackageCompiler.rpath_sysimage()
+    Sys.iswindows() ? `` :
+    Sys.isapple()   ? `-Wl,-rpath,'@loader_path' -Wl,-rpath,'@loader_path/julia'` :
+                      `-Wl,-rpath,\$ORIGIN:\$ORIGIN/julia`
+end
+
 
 PackageCompiler.create_library(
     ".", target_dir;
